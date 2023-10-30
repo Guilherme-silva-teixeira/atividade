8321e3613bb4cbd9a2cdb6dcceae05f69d9d1e75
ShakeDev00 <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2023-10-30 10:53:24 -0300

    Create inicio.java

---
+++ inicio.java
@@ -0,0 +1,12 @@
+package atividade;
+
+public class Atividade 
+{
+
+    public static void main(String[] args) 
+    {
+        login tela = new login();
+        tela.setVisible(true);
+    }
+    
+}
